dc509b935e28771389044a0e167f6f1b8106acc0
Kohei Sato <gandaikouhei@outlook.jp>
2022-10-08 00:16:20 +0900

    カウンター発展問題征服完了　独自工夫も出来た

--- 01_テキスト/01_JavaScriptの関数/main.js
+++ 01_テキスト/01_JavaScriptの関数/main.js
@@ -43,7 +43,10 @@ const genkiFunction = function (number) {
 
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
-
+const minusButton = document.getElementById("minus-button")
+const resetButton = document.getElementById("reset-button")
+const twiceButton = document.getElementById("twice-button")
+const powerButton = document.getElementById("power-button")
 let count = 0
 
 // ボタンをクリックしたときの処理を登録
@@ -55,3 +58,26 @@ plusButton.onclick = function() {
   display.textContent = count
 }
 
+minusButton.onclick = function() {
+  // ここにクリック後の処理を書く
+    // count を更新
+  count -= 1 
+  // count を表示
+  display.textContent = count
+}
+
+resetButton.onclick = function() {
+  count = 0
+  display.textContent = count
+
+}
+
+twiceButton.onclick = function() {
+  count *= 2 
+  display.textContent = count
+}
+
+powerButton.onclick = function() {
+  count **= 2
+  display.textContent = count
+}
