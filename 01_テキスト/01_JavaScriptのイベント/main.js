699e78bfbc48fb32e2eb9f0bcb61dffbf568b716
Kohei Sato <gandaikouhei@outlook.jp>
2022-10-07 23:59:36 +0900

    カウンター無事習得

--- 01_テキスト/01_JavaScriptのイベント/main.js
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -5,4 +5,19 @@ const alertMessage = function () {
   alert("クリックしたね")
 }
 
-button.onclick = alertMessage() // アラートが表示される
+button.onclick = alertMessage // アラートが表示される
+
+// input 要素と input イベント
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function(e) {
+  console.log(e.target.value)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
+
+document.onkeydown = function(e) {
+  console.log(e.key)
+}
